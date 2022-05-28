097aeddd2646b152c6f7e59f3aed68ff6b973010
Latoya Boland <85142599+BolandTech@users.noreply.github.com>
2022-05-28 06:12:26 +0000

    Added Date.java file

---
+++ pre02/Date.java
@@ -0,0 +1,19 @@
+class Date{
+public static void main(String[] args){
+
+String day = "Wednesday";
+int date = 18;
+String month = "February";
+int year = 2007;
+
+System.out.println("The current date is " +day+ " + " +month+ " + " +date+ " + "+year);
+
+// System.out.println("The current date is ");
+// System.out.println(day);
+// System.out.println(month);
+// System.out.println(date);
+// System.out.println(year);
+// System.out.println(".");
+  
+  }
+}
