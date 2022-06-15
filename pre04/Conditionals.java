f8ca97b6a020b186f05955237a567d2367193038
Latoya Boland <85142599+BolandTech@users.noreply.github.com>
2022-06-15 16:42:14 +0000

    Added assignment folders and Conditionals sample file

---
+++ pre04/Conditionals.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Conditionals{ 
+
+public static void main(String[] args){
+  int x; 
+  
+  x = 8;
+
+if (x > 0) {
+    if (x < 10) {
+        System.out.println("positive single digit number.");
+    }
+}
+
+}
+}
+  
